a8a566e7b1dcc2eb6363bdb8edb37a9a672c8d12
Jeongsu0880 <latsyrc900@gmail.com>
2025-01-20 14:04:47 +0900

    #10 resolve

--- src/commands/pass.cpp
+++ src/commands/pass.cpp
@@ -1,6 +1,10 @@
 #include "../../inc/Define.hpp"
 
 std::string Request::execPass(const Server &server, Client *client) {
+
+	if (this->args.size() < 1)
+		return ERR_NEEDMOREPARAMS("PASS");
+
 	if (client->getRegisterStatus()) 
 		return ERR_ALREADYREGISTERED();
 
